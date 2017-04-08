5892c902700fb32366c04baac81b9e8364ad9197
Shaurya Malik <Shaurya Malik>
2017-04-08 15:46:22 +0530

    MapsActivity added

---
+++ Manifesto/app/src/main/java/angelhack/manifesto/MapsActivity.java
@@ -0,0 +1,75 @@
+package angelhack.manifesto;
+
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.identity.intents.Address;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+  public void onSearch(View view){
+      EditText search_text = (EditText)findViewById(R.id.search_bar);
+      String location = search_text.getText().toString();
+
+      List<android.location.Address> addressList=null;
+      if(location != null && !location.equals(""))
+      {
+          Geocoder geocoder = new Geocoder(this);
+          try {
+              addressList = geocoder.getFromLocationName(location, 1);
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+          android.location.Address address = addressList.get(0);
+          LatLng latlng = new LatLng(address.getLatitude(), address.getLongitude());
+          mMap.addMarker(new MarkerOptions().position(latlng).title("Marker"));
+
+          mMap.animateCamera(CameraUpdateFactory.newLatLng(latlng));
+      }
+  }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
+}
